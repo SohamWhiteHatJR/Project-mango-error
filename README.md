90e68cbb4870470bcd5f652cd7ff450297ca1b04
SohamWhiteHatJR <73655345+SohamWhiteHatJR@users.noreply.github.com>
2020-12-22 16:29:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-mango-error
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ mango.js
@@ -0,0 +1,24 @@
+class Mango {
+ constructor(x,y,r){
+     var options={
+        isStatic:true,
+        friction : 1
+        }
+        this.x = x;
+        this.y = y;
+        this.r=r;
+        this.body=Bodies.circle(this.x,this.y,this.r,options);
+        this.image=loadImage("Plucking mangoes/mango.png")
+        World.add(world,this.body)
+       }
+
+    display(){
+    var mangopos=this.body.position
+    push();
+    translate(mangopos.x,mangopos.y);
+    rotate(this.body.angle);
+    imageMode(CENTER);
+    image(this.image,0,0,this.r*2,this.r*2);
+    }
+    pop();
+}
